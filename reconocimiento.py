1708650e4d1c7f24cf133029ccf71896359d17e5
Jose Angel Martin Gonzalez <98955704+joseamg24@users.noreply.github.com>
2022-11-04 18:38:10 +0100

    Interfaz de reconocimiento de voz
    Interfaz de reconocimiento de voz en python mediante la librería speech_recognition.

--- reconocimiento.py
+++ reconocimiento.py
@@ -14,11 +14,14 @@ with sr.Microphone() as micro:
         try:
             text = r.recognize_google(audio,language="es-ES")
             print('\n Has dicho: {}'.format(text))
+            
+            text = text.lower().split()
+            
+            if('finalizar' in text):
+                print("Tarea terminada")
+                break
+
             print('Escuchando.', end='',flush=True)
 
         except:
             print('.', end='',flush=True)
-
-
-
-
