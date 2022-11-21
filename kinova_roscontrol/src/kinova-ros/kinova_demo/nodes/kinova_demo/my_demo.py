c66874786d919bec24d45cfa64d2888b634ecf8e
Daniel Frau Alfaro <98766327+DanielFrauAlfaro@users.noreply.github.com>
2022-11-21 20:19:40 +0100

    Demo para probar en el robot real

---
+++ kinova_roscontrol/src/kinova-ros/kinova_demo/nodes/kinova_demo/my_demo.py
@@ -0,0 +1,90 @@
+#! /usr/bin/env python3
+"""A helper program to test gripper goals for the JACO and MICO arms."""
+
+import roslib; roslib.load_manifest('kinova_demo')
+import rospy
+
+import sys
+
+import actionlib
+import kinova_msgs.msg
+
+import argparse
+
+
+""" Global variable """
+prefix = 'm1n6s300_'  # m1n6s300
+
+
+def gripper_client(finger_positions):
+    """Send a gripper goal to the action server."""
+    action_address = '/' + prefix + 'driver/fingers_action/finger_positions'
+
+    client = actionlib.SimpleActionClient(action_address,
+                                          kinova_msgs.msg.SetFingersPositionAction)
+    client.wait_for_server()
+
+    goal = kinova_msgs.msg.SetFingersPositionGoal()
+    goal.fingers.finger1 = float(finger_positions[0])
+    goal.fingers.finger2 = float(finger_positions[1])
+    
+    # The MICO arm has only two fingers, but the same action definition is used
+    if len(finger_positions) < 3:
+        goal.fingers.finger3 = 0.0
+    else:
+        goal.fingers.finger3 = float(finger_positions[2])
+    client.send_goal(goal)
+    if client.wait_for_result(rospy.Duration(5.0)):
+        return client.get_result()
+    else:
+        client.cancel_all_goals()
+        rospy.logwarn('        the gripper action timed-out')
+        return None
+
+
+# ---------- Arm Joint --------------
+def joint_angle_client(angle_set):
+    """Send a joint angle goal to the action server."""
+    action_address = '/' + prefix + 'driver/joints_action/joint_angles'
+    client = actionlib.SimpleActionClient(action_address,
+                                          kinova_msgs.msg.ArmJointAnglesAction)
+    client.wait_for_server()
+
+    goal = kinova_msgs.msg.ArmJointAnglesGoal()
+
+    goal.angles.joint1 = angle_set[0]
+    goal.angles.joint2 = angle_set[1]
+    goal.angles.joint3 = angle_set[2]
+    goal.angles.joint4 = angle_set[3]
+    goal.angles.joint5 = angle_set[4]
+    goal.angles.joint6 = angle_set[5]
+    goal.angles.joint7 = angle_set[6]
+
+    client.send_goal(goal)
+    if client.wait_for_result(rospy.Duration(20.0)):
+        return client.get_result()
+    else:
+        print('        the joint angle action timed-out')
+        client.cancel_all_goals()
+        return None
+    
+
+def setcurrentJointCommand(feedback):
+    global currentJointCommand
+
+    currentJointCommand_str_list = str(feedback).split("\n")
+    for index in range(0,len(currentJointCommand_str_list)):
+        temp_str=currentJointCommand_str_list[index].split(": ")
+        currentJointCommand[index] = float(temp_str[1])
+        
+def getcurrentJointCommand(prefix_):
+    # wait to get current position
+    topic_address = '/' + prefix_ + 'driver/out/joint_command'
+    rospy.Subscriber(topic_address, kinova_msgs.msg.JointAngles, setcurrentJointCommand)
+    rospy.wait_for_message(topic_address, kinova_msgs.msg.JointAngles)
+    print('position listener obtained message for joint position. ')
+
+
+# Main
+if __name__ == '__main__':
+    print("Starting demo")
