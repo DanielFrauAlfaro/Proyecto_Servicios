7f871d8446ce95062f5cbe502632ae8520b835bf
Jose Angel Martin Gonzalez <98955704+joseamg24@users.noreply.github.com>
2022-11-22 23:10:14 +0100

    Nodo ROS - Interfaz de voz

--- reconocimiento.py
+++ reconocimiento.py
@@ -1,12 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 import speech_recognition as sr
 import os
+import rospy 
+from std_msgs.msg import String
+
+topic = "/voice_ui"
+
+rospy.init_node("voice_ui")
+pub = rospy.Publisher(topic,String,queue_size=10)
+rate = rospy.Rate(10)
 
 r = sr.Recognizer()
 with sr.Microphone() as micro:
 
-    r.adjust_for_ambient_noise(micro)
+    os.system('clear')
+
+    #r.adjust_for_ambient_noise(micro)
     print('Escuchando.', end='',flush=True)
-    while True:
+    while not rospy.is_shutdown():
 
         print(".", end='')
         audio =  r.listen(micro, 10, 3)
@@ -16,12 +28,21 @@ with sr.Microphone() as micro:
             print('\n Has dicho: {}'.format(text))
             
             text = text.lower().split()
+            mensaje = String()
+
+            if("sal" in text):
+                mensaje.data = "sal"
+            elif("pimienta" in text):
+                mensaje.data = "pimienta"
+            elif("azúcar" in text):
+                mensaje.data = "azucar"
             
-            if('finalizar' in text):
-                print("Tarea terminada")
-                break
+            pub.publish(mensaje)
 
             print('Escuchando.', end='',flush=True)
 
         except:
             print('.', end='',flush=True)
+
+        rate.sleep()
+
