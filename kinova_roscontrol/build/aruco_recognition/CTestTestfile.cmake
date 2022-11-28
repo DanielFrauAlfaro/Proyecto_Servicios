c38941c52241b4da03ae4c704aacd40c95f523cf
Daniel Frau Alfaro <98766327+DanielFrauAlfaro@users.noreply.github.com>
2022-11-28 18:14:46 +0100

    Depurar repositorio

--- kinova_roscontrol/build/aruco_recognition/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/daniel/Desktop/Proyecto_Servicios/kinova_roscontrol/src/aruco_recognition
-# Build directory: /home/daniel/Desktop/Proyecto_Servicios/kinova_roscontrol/build/aruco_recognition
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
